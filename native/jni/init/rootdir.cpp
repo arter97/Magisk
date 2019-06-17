6ee807e382529c843933620d047c44f4dbd1285f
Park Ju Hyung <qkrwngud825@gmail.com>
2019-06-18 02:17:30 +0900

    Limit random socket name generation to 4 to 12 char
    https://i.imgur.com/iWKad22.jpg
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- native/jni/init/rootdir.cpp
+++ native/jni/init/rootdir.cpp
@@ -27,7 +27,7 @@ static void patch_socket_name(const char *path) {
 	mmap_rw(path, buf, size);
 	for (int i = 0; i < size; ++i) {
 		if (memcmp(buf + i, MAIN_SOCKET, sizeof(MAIN_SOCKET)) == 0) {
-			gen_rand_str(buf + i, (rand() % 32) + 32);
+			gen_rand_str(buf + i, (rand() % 8) + 4);
 			i += sizeof(MAIN_SOCKET);
 		}
 	}
